15c819f2993d538e6fd56aa053a7ec728d0ba978
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-11-16 08:54:59 -0300

    bug fixed

--- organo/src/App.js
+++ organo/src/App.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
-// import Time from './componentes/Time';
+import Time from './componentes/Time';
 
 function App() {
 
@@ -56,13 +56,13 @@ function App() {
       <Banner />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
 
-      {/* {times.map(time => <Time
+      {times.map(time => <Time
         key={time.nome}
         nome={time.nome}
         corPrimaria={time.corPrimaria}
         corSecundaria={time.corSecundaria}
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
-      />)} */}
+      />)}
 
     </div>
   );
